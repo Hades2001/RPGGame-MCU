253955808f1c4b70aef5f3d52911c2e706b7159c
Hades2001 <1521377294@qq.com>
2020-04-17 18:14:53 +0800

    test map file to display

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,56 @@
 #include <M5Stack.h>
-#include <WiFi.h>
-#include <HTTPClient.h>
+#include "esp_spi_flash.h"
 
 #include "mappacklib.h"
+
+extern uint16_t map_maplayer0[30][30];
+extern uint16_t map_maplayer1[30][30];
+extern uint16_t map_maplayer2[30][30];
 
 TFT_eSprite Disbuff = TFT_eSprite(&M5.Lcd);
 
 const char *WifiSSID = "M5-5G";
 const char *WifiPWD = "Office@888888";
 
+uint8_t imagedata[576];
 
 void drawMapPack(uint16_t x, uint16_t y, uint16_t group, uint16_t mapPackData)
 {
-    //uint8_t (*mapptr)[1152] = (uint8_t (*)[1152])mapliblist[mapgroupbuff[group][0]];
-    for( int posY = 0; posY < 24; posY ++ )
+    if( !( mapPackData & 0x8000 )) return;
+
+    uint16_t mapnumber = ( mapPackData >> 12 ) & 0x07;
+
+    uint16_t *colorindex = indexList[mapgroupbuff[group][mapnumber]];
+    spi_flash_read(0x420000 + addrList[mapgroupbuff[group][mapnumber]] + ( mapPackData & 0x3ff ) * 576,imagedata,sizeof(uint8_t) * 576);
+    for (int i = 0; i < 576; i++)
     {
-        for( int posX = 0; posX < 24; posX ++ )
+        if( imagedata[i] == 0xff ) continue;
+        Disbuff.drawPixel(  x + i % 24,
+                            y + i / 24,
+                            colorindex[imagedata[i]]);
+    }
+}
+uint16_t *maplayerbuff[3] = {
+    (uint16_t*) map_maplayer0,
+    (uint16_t*) map_maplayer1,
+    (uint16_t*) map_maplayer2,
+};
+void drawmap( int posx, int posy)
+{
+    uint16_t (*maplayerptr)[30] = nullptr;
+    for( int n = 0; n < 3; n++ )
+    {
+        maplayerptr = (uint16_t (*)[30])maplayerbuff[n];
+        for( int y = 0 ; y < 10 ; y++ )
         {
-            Disbuff.drawPixel(x + posX,y + posY,Outside_A1_index[map_Outside_A1data[mapPackData][posY * 24 + posX]]);
+            for( int x = 0 ; x < 13 ; x++ )
+            {
+                drawMapPack(x*24,y*24,2,maplayerptr[y+posy][x+posx]);
+            }
         }
     }
-    Disbuff.pushSprite(0,0);
-    /*
-    if( mapPackData & 0x8000 )
-    {
 
-    }
-    */
+    Disbuff.pushSprite(0,0);
 }
 
 void setup()
@@ -44,68 +68,64 @@ void setup()
     Disbuff.setSwapBytes(true);
 
     Disbuff.pushSprite(0,0);
-    //Disbuff.pushImage(0,0,48,48,(uint16_t*)qwbuff[0]);
-    //Disbuff.pushSprite(136, 96);
 
     Serial.println("Init Wifi");
-    
-    drawMapPack( 0 ,0 ,1 ,0 );
-    drawMapPack( 24,0 ,1 ,1 );
-    drawMapPack( 48,0 ,1 ,2 );
-    /* 
-    WiFi.mode(WIFI_STA);
-    WiFi.setSleep(false);
-    WiFi.begin(WifiSSID, WifiPWD);
-
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(500);
-        Serial.print('.');
-    }
-    Serial.println("Wifi Connected");
-    */
+    drawmap(0,0);
+    Disbuff.pushSprite(0,0);
 
 }
 // start | select | a | b | down | right | left | up
 char bitbuff[9];
 uint8_t dir = 0,state = 0;
+int posx = 0, posy = 0;
 void loop()
 {
     // put your main code here, to run repeatedly:
     M5.update();
     Wire.requestFrom(0x08,1);
     uint8_t data = ~Wire.read();
-    if( data & 0x08 )
+
+    if( data & 0x08 )       //down
     {
         dir = 0;
         state ++;
         state = state % 3;
+        posy ++;
+        posy = posy > 20 ? 20 : posy;
     }
-    else if( data & 0x04 )
+    else if( data & 0x04 )  //right
     {
         dir = 2;
         state ++;
         state = state % 3;
+        posx ++;
+        posx = posx > 17 ? 17 : posx;
     }
-    else if( data & 0x02 )
+    else if( data & 0x02 )  //left
     {
         dir = 1;
         state ++;
         state = state % 3;
+        posx --;
+        posx = posx < 0 ? 0 : posx;
     }
-    else if( data & 0x01 )
+    else if( data & 0x01 )  //up
     {
         dir = 3;
         state ++;
         state = state % 3;
+        posy --;
+        posy = posy < 0 ? 0 : posy;
     }
     else
     {
         state = 1;
     }
+    drawmap(posx,posy);
+    Disbuff.pushSprite(0,0);
 
     //Disbuff.pushImage(120,120,48,48,(uint16_t*)qwbuff[dir * 3 + state]);
-    Disbuff.pushSprite(0, 0);
+    //Disbuff.pushSprite(0, 0);
 
-    delay(150);
+    //delay(10);
 }
